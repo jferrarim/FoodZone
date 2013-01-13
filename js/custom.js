eca2abf73f238b11367c1bed151961a4f2c1893b
Joshua Ferreira <Jferreir@nyit.edu>
2013-01-13 01:04:32 -0500

    Blog Front End
    HTML/CSS and Javascript completed for blog page

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,31 @@
 */
 (function($){
 
-	
+	// Blog Subnav
 	$('.categories li a').bind('click', (function(e) {
 		$('li a').removeClass('selected');
 		$(this).addClass('selected');
+		return false;
 	}));
+
+
+	// Accordion
+	var allPanels = $('.accordion div div').hide();
+    
+  $('.accordion a').bind('click', (function(e) {
+  
+    if( $(this).hasClass('down') ){
+    	$(this).next().slideUp();
+    	$(this).removeClass('down');
+    	$(this).find('i').removeClass('icon-minus').addClass('icon-plus');
+    }else{
+    	$(this).next().slideDown();
+    	$(this).addClass('down');
+    	$(this).find('i').removeClass('icon-plus').addClass('icon-minus');
+    }
+
+    return false;
+
+  }));
 
 })(jQuery);
